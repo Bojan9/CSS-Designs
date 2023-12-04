42f96df6139beefdb1474fd0f168a8a7f07e32a0
Bojan9 <9spasovski@gmail.com>
2023-12-04 22:29:10 +0100

    added styles

--- 01-Login/script.js
+++ 01-Login/script.js
@@ -1,11 +1,14 @@
 const container = document.getElementById('container');
 const registerBtn = document.getElementById('register');
 const loginBtn = document.getElementById('login');
+const signInForm = document.getElementById('signInForm');
 
 registerBtn.addEventListener('click', () => {
     container.classList.add("active");
+    signInForm.classList.add("hidden");
 });
 
 loginBtn.addEventListener('click', () => {
     container.classList.remove("active");
+    signInForm.classList.remove("hidden");
 });
